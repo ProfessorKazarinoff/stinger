ded5a41a047d4386ba50a7c48de2aba8d41acb21
Peter Kazarinoff <peter.kazarinoff@pcc.edu>
2020-05-17 22:16:55 -0700

    moving rectangle works

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # stinger
+
+!! NOT READY FOR PRODUCTION !!
+
 A project to build a stinger transition for Twitch streaming using Python and OpenCV
--- matplotlib_rectangle.py
+++ matplotlib_rectangle.py
@@ -1,41 +1,29 @@
-# matplotlib_animation.py
-"""
-A Python script to run a Matplotlib animation to build a stinger transition
-"""
-
-#import necessary packages
 import numpy as np
-from numpy import pi, sin, cos, sqrt
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-import matplotlib.patches as mpatches
-from matplotlib.collections import PatchCollection
+from matplotlib import pyplot as plt
+from matplotlib import animation
 
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
-import numpy as np
+nx = 1080
+ny = 720
 
-# Create figure and axes
-fig,ax = plt.subplots()
-x = np.arange(0,1280,1)
-y = (720/1280)*x
-
-
-# Create a Rectangle patch
-rect = patches.Rectangle((50,100),50,20,angle=50,linewidth=0,edgecolor='b',facecolor='b')
-
-# Add the patch to the Axes
-ax.set_xlim([0,1280])
-ax.set_ylim([0,720])
-patch = ax.add_patch(rect)
-
+fig = plt.figure()
+plt.axis([0,nx,0,ny])
+ax = plt.gca()
+ax.set_aspect(1)
+x = np.arange(0,10,1)
+y = x
 def init():
-    patch.rect
+    # initialize an empty list of cirlces
+    return []
 
 def animate(i):
-    patch.rect((x[i],y[i]),50,20,angle=i,linewidth=0,edgecolor='b',facecolor='b')
-    return patch
+    # draw circles, select to color for the circles based on the input argument i. 
+    someColors = ['r', 'b', 'g', 'm', 'y']
+    patches = []
+    patches.append(ax.add_patch(plt.Rectangle(
+        xy=(i,0.5*i),width=5*i,height=5*i,angle=.3*i
+        )))
+    return patches
 
-ani = animation.FuncAnimation(fig, animate, init_func=init, frames=len(x), interval=40, blit=True, repeat=False)
-
+anim = animation.FuncAnimation(fig, animate, init_func=init,
+                               frames=1080, interval=10, blit=True)
 plt.show()
